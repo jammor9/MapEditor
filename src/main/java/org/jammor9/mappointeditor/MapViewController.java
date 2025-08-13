1dc9dbeafe0cd2179b9bcb8068fbd1b82873842f
jammor9 <jamesamorrison2005@googlemail.com>
2025-08-13 21:02:19 +0100

    Zoomable Scroll Pane Added +Added ZoomableScrollPane, which allows for zooming into and panning the loaded image
    Credit for the ZoomableScrollPane class to https://stackoverflow.com/questions/39827911/javafx-8-scaling-zooming-scrollpane-relative-to-mouse-position

--- src/main/java/org/jammor9/mappointeditor/MapViewController.java
+++ src/main/java/org/jammor9/mappointeditor/MapViewController.java
@@ -4,26 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ScrollEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import org.jammor9.mappointeditor.models.MapModel;
 
 public class MapViewController {
-    @FXML public ImageView mapImageView;
-    public ScrollPane mapViewScrollPane;
+    @FXML public ZoomableScrollPane mapViewScrollPane;
     private MapModel mapModel = MapModel.getInstance();
+    private ImageView mapImageView;
+
 
     @FXML
     private void initialize() {
-        mapImageView.setImage(mapModel.getMapImage());
+        this.mapImageView = new ImageView(mapModel.getMapImage());
+        mapViewScrollPane.setTarget(mapImageView);
     }
 
     public void loadMap(ActionEvent actionEvent) {
         mapImageView.setImage(mapModel.getMapImage());
-    }
-
-    public void zoomPane(ScrollEvent scrollEvent) {
-        System.out.println(scrollEvent.getDeltaY());
-        if (scrollEvent.getDeltaY() > 0) mapImageView.setScaleX(mapImageView.getScaleX() + 1);
-        else mapImageView.setScaleX(mapImageView.getScaleX() - 1);
     }
 }
